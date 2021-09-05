728da21a6ed929a7b9ad6ebbecc0cc4a7689bf2f
mgavillo <mgavillo@student.42.fr>
2021-09-05 22:57:03 +0200

    still need something lol

--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import argparse
+import math
 my_dict = {'M': 1, "B": 0}
 
 class Activations:
@@ -13,8 +14,8 @@ class NeuralNetwork:
         self.b = []
         self.phi = []
         self.mu = []
-        self.eta = 1 #set up the proper Learning Rate!!
-        self.epochs = 1000
+        self.eta = 0.5 
+        self.epochs = 10000
     
     def add(self, layer):
         self.layers.append(layer)
@@ -24,7 +25,8 @@ class NeuralNetwork:
         for i in range(0, len(self.layers)):
             if i != 0:
                 self.layers[i].weights = np.random.rand(self.layers[i].n_neurons, self.layers[i - 1].n_neurons)
-                self.layers[i].bias = np.random.rand(self.layers[i].n_neurons)
+                self.layers[i].bias = np.ones(self.layers[i].n_neurons)
+                # self.layers[i].bias = np.random.rand(self.layers[i].n_neurons)
 
     def sigmoid(self, X):
         z=np.array(X,dtype=np.float32)
@@ -36,55 +38,75 @@ class NeuralNetwork:
 
     def gradient_error(self, Y_train):
         Y_train = np.array([my_dict[value] for value in Y_train])
-        print("\n")
-        print(Y_train)
+        # print("\n")
+        # print(Y_train)
 
-        print(Y_train.shape)
+        # print(Y_train.shape)
         return Y_train - self.layers[3].neurons
 
     # def update_weights(self):
     #     for i in range(0, len(self.layers)):
     #         if i != 0:
     #             self.layers[i].weights = np.dot(sigmoid(self.layers[i - 1].neurons).T, )
+    def backpropagation(self, Y_train):
+        error = self.gradient_error(Y_train)
+
+        for layer in range(len(self.layers) - 1, 0, -1):
+            slope = self.derivative_sigmoid(self.layers[layer].neurons)
+            d_layer = error * slope
+            # print("-------------------------------")
+            # print(self.layers[layer].weights)
+            # print(self.layers[layer - 1].neurons.T.shape)
+            # print(d_layer.shape)
+            # print(np.dot(d_layer, self.layers[layer - 1].neurons.T).shape)
+            self.layers[layer].weights = self.layers[layer].weights + np.dot(d_layer, self.layers[layer - 1].neurons.T) * self.eta
+            # print("then, ",  self.layers[layer].weights)
+            self.layers[layer].bias -= np.sum(d_layer, axis = 1) * self.eta
+            error = np.dot(self.layers[layer].weights.T, d_layer)
+
+    def print_shapes(self):
+        for layer in range(0, len(self.layers)):
+            print("LAYER ", layer)
+            print(self.layers[layer].neurons.shape)
+            if( layer != 0):
+                print(self.layers[layer].weights.shape)
+                print(self.layers[layer].bias.shape)
 
     def fit(self, X_train, Y_train):
+        # print(self.layers[1].weights)
+        # print(self.layers[1].bias)
+        # pass
+        print("###############################################")
+        print(self.layers[0].neurons)
         for epoch in range(self.epochs):
             # print(epoch)
-            for i in range(0, len(self.layers)):
-                if i != 0:
-                    print("neurons = ", self.layers[i - 1].neurons.T.shape)
-                    print("weights = ", self.layers[i].weights.T.shape)
-                    input = np.dot(self.layers[i - 1].neurons.T, self.layers[i].weights.T) + self.layers[i].bias
-                    self.layers[i].neurons = self.sigmoid(input.T)
-                    print(input.shape)
-            error = self.gradient_error(Y_train)
 
-            slope_output_layer = self.derivative_sigmoid(self.layers[3].neurons).T
-            slope_hidden_layer1 = self.derivative_sigmoid(self.layers[1].neurons).T
-            slope_hidden_layer2 = self.derivative_sigmoid(self.layers[2].neurons).T
+            # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            # print(self.layers[1].neurons)
 
-            d_output = error * slope_output_layer
-            print("cucul")
-            print(d_output.shape)
-            print(self.layers[3].weights.shape)
-            Error_at_hidden_layer2 = np.dot(d_output, self.layers[3].weights.T)
-            
-            print(Error_at_hidden_layer2.shape)
-            print(slope_hidden_layer2.shape)
-            d_hiddenlayer2 =  Error_at_hidden_layer2 * slope_hidden_layer2
-            
-            Error_at_hidden_layer1 = np.dot(d_hiddenlayer2.T , self.layers[2].weights)
-            d_hiddenlayer1 = Error_at_hidden_layer1 * slope_hidden_layer1
-
-            self.layers[3].weigths = self.layers[3].weights + np.dot(self.layers[3].neurons, d_output) * self.eta
-            self.layers[2].weights = self.layers[2].weights + np.dot(self.layers[2].neurons, d_hiddenlayer2) * self.eta
-            self.layers[1].weights = self.layers[1].weights + np.dot(self.layers[1].enurons, d_hiddenlayer1) * self.eta
-            
-            self.layers[3].bias += sum(d_ouptut, axis=0) * self.eta
-            self.layers[2].bias += sum(d_hiddenlayer2, axis=0) * self.eta
-            self.layers[1].bias += sum(d_hiddenlayer1, axis=0) * self.eta
-        
-
+            for i in range(1, len(self.layers)):
+                
+                input = np.dot(self.layers[i - 1].neurons.T, self.layers[i].weights.T) + self.layers[i].bias
+                if(epoch == self.epochs - 1):
+                    print("layer : ", i)
+                    print(np.dot(self.layers[i - 1].neurons.T, self.layers[i].weights.T))
+                    print(input)
+                self.layers[i].neurons = self.sigmoid(input.T)
+                # print(self.layers[i].weights)
+                # print(self.layers[i].bias)
+                if(epoch == self.epochs - 1):
+                    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~é")
+                    print(self.layers[i].neurons)
+                # print(input.shape)
+            # self.print_shapes()
+            self.backpropagation(Y_train)
+            # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~é")
+            # print(self.layers[3].neurons)
+            # print(self.layers[3].neurons)
+        print("RESULTS")
+        print(self.layers[3].neurons)
+        # print(Y_train)
+        print(self.gradient_error(Y_train))
 
 #output layer is shape of number of labels
 
@@ -105,25 +127,73 @@ class Layer(NeuralNetwork):
     def calc_bias():
         self.bias = np.random.rand(1)
 
-    def forward(self, input):
-        return input
+def calc_count_mean(data):
+    '''
+    for each feature, calulate count without blank input
+    then calculate average
+    '''
+    count = [0 for x in range(data.shape[1])]
+    mean = [0 for x in range(data.shape[1])]
+    print(data.shape[1])
+    for column in range(0, data.shape[1]):
+        count[column] = np.sum(data[:][column])
+        for d in range(data.shape[0]):
+            print(data[d][column])
+            x = data[d][column]
+            if not math.isnan(x):
+                mean[column] += x
+        mean[column] /= count[column]
+    return count, mean
 
-    def backward(self, input, grad_output):
-        num_units = input.shape[1]
-        d_layer_d_input = np.eye(num_units)
-        return np.dot(grad_output, d_layer_d_input)
+def calc_std(data, count, mean):
+    '''
+    for each feature, calculate standart deviation, a measure of
+    the spread of a distribution
+    sdt = sqrt(mean(abs(x - x.mean())**2))
+    '''
+    std = [0 for x in range(data.shape[1])]
+    for column in range(0, data.shape[1]):
+        deviations = []
+        for d in range(0, data.shape[0]):
+            x = data[d][column]
+            if not math.isnan(x):
+                deviations.append((x - mean[column]) ** 2)
+        variance = sum(deviations) / count[column]
+        std[column] = math.sqrt(variance)
+    return std 
+
+def normalize(count, mean, std, data):
+    for column in range(0, data.shape[1]):
+        for index in range(0, data.shape[0]):
+            # print(data[1][0])
+            # print("column = ", column, "index = ", index)
+            # print(type(data[index][column]))
+            # print(data[index][column])
+            # print(type(mean[column]))
+            # print(type(std[column]))
+            data[index][column] = (data[index][column] - mean[column])/ std[column]
 
 def preprocess_data(data):
     data = data.to_numpy()
     X = data[:, 2:]
+    count, mean = calc_count_mean(X)
+    std = calc_std(X, count, mean)
+    print(X)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~é")
+    normalize(count, mean, std, X)
+    print(X)
     Y = data[:, 1]
     n = int(X.shape[0] / 10)
     X_train = X[n:, :]
     X_test = X[:n+1, :]
     Y_train = Y[n:]
     Y_test = Y[:n + 1]
+    print(X[0][0])
+    print(X[1][0])
     print(Y.shape)
     print(Y_test.shape)
+
+
     return(X_train, Y_train, X_test, Y_test)
 
 if __name__ == "__main__":
